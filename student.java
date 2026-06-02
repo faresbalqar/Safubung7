6c2eee001ec89ef302989ab7513050907c7ee8b7
faresbalqar <faresbalqar73@gmail.com>
2026-06-02 14:21:18 +0200

    add student class for task 1

---
+++ student.java
@@ -0,0 +1,52 @@
+public class student {
+    private String Vorname;
+    private String Nachname;
+    private int MatrNr;
+    private String Studiengang; 
+    private int Semester;
+
+
+
+    ///konstruktor
+     
+
+    public student(int matNr, int fsemester, String vname, String nname){
+        this.MatrNr = matNr;
+        this.Semester = fsemester;
+        this.Vorname = vname;
+        this.Nachname = nname;
+    }
+
+
+
+    public String getVorname() {
+        return Vorname;
+    }
+
+
+
+    public String getNachname() {
+        return Nachname;
+    }
+
+
+
+    public int getMatrNr() {
+        return MatrNr;
+    }
+
+
+
+    public String getStudiengang() {
+        return Studiengang;
+    }
+
+
+
+    public int getSemester() {
+        return Semester;
+    }
+
+    //// getter und setter
+    
+    
