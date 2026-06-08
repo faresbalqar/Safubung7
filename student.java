74853c4f09fedb958297d2eb223a1070f9e32fc6
chtcherry <Yuliya.Chtcherbo@Student.HTW-Berlin.de>
2026-06-09 01:26:06 +0200

    add new changes to all the files

--- student.java
+++ student.java
@@ -4,12 +4,10 @@ public class Student {
     private int MatrNr;
     private String Studiengang; 
     private int Semester;
-    
 
     ///konstruktor
-     
-
-    public student(int matNr, int fsemester, String vname, String nname){
+    
+    public Student(int matNr, int fsemester, String vname, String nname){
         this.MatrNr = matNr;
         this.Semester = fsemester;
         this.Vorname = vname;
@@ -18,35 +16,51 @@ public class Student {
 
     //// getter und setter
 
-    
     public String getVorname() {
         return Vorname;
     }
-
-
 
     public String getNachname() {
         return Nachname;
     }
 
-
-
     public int getMatrNr() {
         return MatrNr;
     }
-
-
 
     public String getStudiengang() {
         return Studiengang;
     }
 
-
-
     public int getSemester() {
         return Semester;
     }
 
+    public void setVorname(String vorname) {
+        this.Vorname = vorname;
+    }
+
+    public void setNachname(String nachname) {
+       this.Nachname = nachname;
+    }
+
+    public void setMatrNr(int matrNr) {
+        this.MatrNr = matrNr;
+    }
+
+    public void setStudiengang(String studiengang) {
+        this.Studiengang = studiengang;
+    }
+
+    public void setSemester(int semester) {
+        this.Semester = semester;
+    }
+
+    public void printStudent(){
+        System.out.println("Vorname : " + this.Vorname);
+        System.out.println("Nachname : " + this.Nachname);
+        System.out.println("MatrNr : " + this.MatrNr);
+        System.out.println("Semester : " + this.Semester);
+    }
     
-    
-    
+}   
